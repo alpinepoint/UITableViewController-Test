e12ecfecc7dd22713b85b6710e8c20ef378644cd
Jorge Corona <jorgecorona@MacBook-Air.local>
2014-05-21 08:44:33 -0500

    Initial Commit

---
+++ UITableViewController Challenge Solution/JCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JCAppDelegate.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Jorge Corona on 5/21/14.
+//  Copyright (c) 2014 Alpine Point. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UITableViewController Challenge Solution/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UITableViewController Challenge Solution
+//
+//  Created by Jorge Corona on 5/21/14.
+//  Copyright (c) 2014 Alpine Point. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JCAppDelegate class]));
+    }
+}
